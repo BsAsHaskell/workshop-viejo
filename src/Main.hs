f2395b5a02e13b2dbef4c3b5b27b777b166ba9a3
clrnd <welcometothechango@gmail.com>
2015-05-29 18:17:19 -0300

    paso2 init

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,29 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Data.Csv               (HasHeader(..), decode)
+import Data.ByteString.Lazy   as B
+import Data.Vector            as V (Vector(..), take)
+import Data.Aeson             (object, (.=))
+import System.FilePath        ((</>))
 import Web.Scotty
+import Control.Monad.IO.Class
 
 main :: IO ()
 main = scotty 3000 $ do
-  get "/" $ do
-      html "Hello World!"
+
+    get "/" $ do
+        html "Hello World!\n"
+
+    get "/episodes/" $ do
+        csv <- liftIO $ getCSV "episode"
+        case csv of
+            Left err -> json $ object ["error" .= err]
+            Right vals -> do
+                liftIO $ print $ V.take 10 vals
+                json $ object ["ok" .= ("yes" :: String)]
+
+getCSV :: String -> IO (Either String (Vector (Vector ByteString)))
+getCSV name = do
+    f <- B.readFile $ "csvs/" </> name ++ ".csv"
+    return $ decode HasHeader f
