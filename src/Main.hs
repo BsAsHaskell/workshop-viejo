5a094d378a64f9719acbdeab8e4857b365e10e90
clrnd <welcometothechango@gmail.com>
2015-09-03 13:16:56 -0300

    cleaning up

--- src/Main.hs
+++ src/Main.hs
@@ -13,7 +13,6 @@ import           Network.Wai.Middleware.RequestLogger
 
 import           App.Model
 import           App.Csv
-import           App.Join
 import           App.Json
 
 main :: IO ()
@@ -40,35 +39,3 @@ main = scotty 3000 $ do
             Right vals -> do
                 let l = V.find (\x -> episodeId x == id') vals
                 json $ l
-
-    get "/speech/:speaker" $ do
-        sentences <- liftIO $ getCSV "sentence"
-        utterances <- liftIO $ getCSV "utterance"
-
-        spk' <- param "speaker"
-        case (,) <$> sentences <*> utterances of
-            Left err -> json $ jsonError err
-            Right (ss, us) -> do
-                let us' = V.filter (\u -> utteranceSpeaker u == spk') us
-                let speech = speechJoin ss us'
-
-                json $ speech
-
-    get "/wordfreq/:speaker" $ do
-        sentences <- liftIO $ getCSV "sentence"
-        utterances <- liftIO $ getCSV "utterance"
-
-        spk' <- param "speaker"
-        case (,) <$> sentences <*> utterances of
-            Left err -> json $ jsonError err
-            Right (ss, us) -> do
-                let us' = V.filter (\u -> utteranceSpeaker u == spk') us
-                let speech = speechJoin ss us'
-                let word_freqs = S.filter (>50) $ wordFreq speech
-
-                json $ word_freqs
-
-    get "/static/:filename" $ do
-        name <- param "filename"
-        f <- liftIO $ T.readFile $ "web" </> name
-        html f
